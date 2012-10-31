940dc84321f77e941bafc9a3d09de75b897af632
gshwang <jdev2dev@gmail.com>
2012-10-31 18:44:41 +0900

    create new project

---
+++ sample_jwebsocket_client/WebContent/js/SampleClientPlugIn.js
@@ -0,0 +1,49 @@
+jws.SampleClientPlugIn = {
+	// if namespace is changed update server plug-in accordingly!
+	NS: "com.sample.SamplePlugIn",
+
+	//Method is called when a token has to be progressed
+	processToken: function( aToken ) {
+		console.log("[processToken][ns]["+aToken.ns+"]:["+jws.SampleClientPlugIn.NS+"]");
+	    // check if namespace matches
+	    if( aToken.ns == jws.SampleClientPlugIn.NS ) {
+	      // if it's an answer for the request "getAuthorName"
+	      if( aToken.reqType == "getAuthorName" ) {
+	    	  alert( "This Tutorial is done by: " + aToken.name );
+	      }
+	      // if it's an answer for the request "calculate"
+	      else if( aToken.reqType == "calculate" ) {
+	    	  alert( "calculated Number is: " + aToken.calNumber );
+		  }
+	    }
+	  },
+
+	  //Method is called from the button "Author"
+	  //to send a request to the jwebsocketserver-> LauridsPlugIn
+	  requestAuthorName: function( aOptions ) {
+	    if( this.isConnected() ) {
+	       //create the request token
+	      var lToken = {
+	         ns: jws.SampleClientPlugIn.NS,
+	         type: "getAuthorName"
+	      };
+	      console.log("asking for Author Name...");
+	      this.sendToken( lToken,  aOptions );//send it
+	    }
+	  },
+
+	  calculateMyNumber: function(inputNumber, aOptions ) {
+		    if( this.isConnected() ) {
+		       //create the request token
+		      var lToken = {
+			     ns: jws.SampleClientPlugIn.NS,
+			     type: "calculate",
+			     myNumber: inputNumber//add the input Number to our token
+		      };
+		      console.log("sending calculation request for:"+inputNumber);
+		      this.sendToken( lToken,  aOptions );//send it
+		    }
+		}
+};
+//add the client PlugIn
+jws.oop.addPlugIn( jws.jWebSocketTokenClient, jws.SampleClientPlugIn );
---
+++ sample_jwebsocket_client/WebContent/js/js_code.js
@@ -0,0 +1,72 @@
+var websocketID = "meinTest",
+websocketClient = null,
+wsUsername = null,
+wsPassword = null;
+
+//Method is called when the page is loaded
+function initPage() {
+	// check if WebSockets are supported by the browser
+	if( jws.browserSupportsWebSockets() ) {
+		console.log("Websockets are supported");
+		// instaniate new TokenClient, either JSON, CSV or XML
+		websocketClient = new jws.jWebSocketJSONClient();		
+		logon();//call the logon Method
+	} else {
+		console.log("Websockets are NOT supported");
+		
+	}
+}
+
+//Method is called when the user clicks the "calculate" button
+function requestResult(){
+	myNumber=parseInt(document.getElementById("inputNumber").value);//get the number
+	websocketClient.calculateMyNumber(myNumber);//call the calculate method from LauridsClientPlugIn.js
+}
+
+function logon() {
+	var wsURL = jws.getDefaultServerURL();// get the default server url
+	console.log("Connecting to " + websocketID + " at " + wsURL + "..." );
+
+	//get the guest username and password
+	wsUsername=jws.GUEST_USER_LOGINNAME;
+	wsPassword=jws.GUEST_USER_PASSWORD;
+
+	// try to establish the connection to jWebSocket server
+	websocketClient.logon( wsURL, wsUsername, wsPassword, {
+		OnOpen: function( aEvent ) {
+			console.log("Connection to " + websocketID + " established." );
+		},
+
+		// OnMessage callback
+		OnMessage: function( aEvent, aToken ) {
+			console.log(( aToken ? aToken.type : "-" ) + ": " +	aEvent.data);
+
+			if( websocketClient.isLoggedIn() ) {
+				console.log("User is authenticated");
+			} else {
+				console.log("User is connected");
+			}
+			console.log(websocketClient.getId() + "&nbsp;"	+ ( jws.browserSupportsNativeWebSockets ? "(native)" : "(flashbridge)" ));
+			if( aToken ) {
+				// is it a response from a previous request of this client?
+				if( aToken.type == "response" ) {
+					if( aToken.reqType == "login" ) {// figure out of which request
+						if( aToken.code == 0 ) {
+							console.log("Welcome '" + aToken.username + "'" );
+						}else {
+							console.log("Error logging in '" + eUsername.value + "': " + aToken.msg );
+						}
+					}
+				// is it an event w/o a previous request ?
+				}else if( aToken.type == "goodBye" ) {
+					console.log("good bye (reason: " + aToken.reason + ")!" );
+				}
+			}
+		},
+		// OnClose callback
+		OnClose: function( aEvent ) {
+			console.log("Disconnected from " + websocketID + ".");
+		}
+
+	});
+}
